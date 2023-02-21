52fb801c0456b1ebd4a63a684597e9dc18dd6cce
Matthéo PERELLE <mattheo.perelle@gmail.com>
2023-02-21 10:28:23 +0100

    feat(sql): jeu de données mock.sql

---
+++ sql/mock.sql
@@ -0,0 +1,6 @@
+use chatpot;
+INSERT INTO chat(auteur, contenu) VALUE ('Matthéo PERELLE', 'Bonjour la plouc');
+INSERT INTO chat(auteur, contenu) VALUE ('Matthéo PERELLE', 'Comment tu t''appelles ?');
+INSERT INTO chat(auteur, contenu) VALUE ('Anna NIETO', 'En-chantier, je m''appelle teuse et toi-ture ?');
+INSERT INTO chat(auteur, contenu) VALUE ('Matthéo PERELLE', 'Et moi-ssonneuse, Et lui-le d''olive c''est mon frere');
+INSERT INTO chat(auteur, contenu) VALUE ('Anna NIETO', 'Et moi ma mère-cédès');

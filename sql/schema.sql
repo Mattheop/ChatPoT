608b4eeb593bd6345a24ed5427e959c349f4a691
Matthéo PERELLE <mattheo.perelle@gmail.com>
2023-02-21 10:21:16 +0100

    refact(sql): reformat schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,7 +1,7 @@
---Création de la base de données
+-- Création de la base de données
 CREATE DATABASE chatpot;
 
---Création des tables
+-- Création des tables
 CREATE TABLE chatpot.chat (
   id INT NOT NULL AUTO_INCREMENT, 
   date_envoi DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, 

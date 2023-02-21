d3dc0490fe13797c430ddf2c5a15a5f04ced4b76
Anna NIETO <anna.nieto@etu.iut-tlse3.fr>
2023-02-21 10:16:12 +0100

    feat(SQL) : création schema sql avec la table chat

---
+++ sql/schema.sql
@@ -0,0 +1,12 @@
+--Création de la base de données
+CREATE DATABASE chatpot;
+
+--Création des tables
+CREATE TABLE chatpot.chat (
+  id INT NOT NULL AUTO_INCREMENT, 
+  date_envoi DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+  auteur VARCHAR(30) NOT NULL, 
+  contenu VARCHAR(255) NOT NULL, 
+  PRIMARY KEY (id)
+) ENGINE = InnoDB;
+

3dd5ae41299c0d0e931dea75616e7d02a654cd3b
Matthéo PERELLE <mattheo.perelle@gmail.com>
2023-03-14 09:54:49 +0100

    feat(connexion): ajout du systeme de connexion
    (cherry picked from commit d9f20a5b65b577a836d188e0d8c8a89179f16e7e)

--- www/afficher.php
+++ www/afficher.php
@@ -1,3 +1,14 @@
+<?php
+
+if (empty($_GET['user'])) {
+    header("Location: login.php");
+    die();
+}
+
+$user = $_GET['user'];
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -6,83 +17,90 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ChatPoT</title>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    <style>
+        .me {
+            color: red;
+        }
+    </style>
     <script defer>
-        $(()=> {
+        $(() => {
             const conversation = $("#conversation");
             const messageInput = $("#message");
             const messageForm = $("#message-form");
             const nameInput = $("#name");
+            const user = nameInput.val();
 
             const messagesIds = [];
 
             const renderMessage = (message) => {
-                if(messagesIds.includes(parseInt(message.id))){
+                if (messagesIds.includes(parseInt(message.id))) {
                     return;
                 }
                 messagesIds.push(parseInt(message.id));
                 console.log(messagesIds)
-                conversation.append($(`<li data-chat-id='${message.id}'>${message.contenu}</li>`));
+                conversation.append($(`<li data-chat-id='${message.id}' class="${user === message.auteur ? "me" : "other"}" ><strong>${message.contenu}</strong> par ${message.auteur}</li>`));
             }
 
             $.ajax({
-                url:"./recuperer.php",
+                url: "./recuperer.php",
                 method: "GET"
             })
-            .done((response)=>{
-                const messages = JSON.parse(response).data;
-                messages.forEach(renderMessage);
-            });
+                .done((response) => {
+                    const messages = JSON.parse(response).data;
+                    messages.forEach(renderMessage);
+                });
 
-            messageForm.submit((event)=>{
+            messageForm.submit((event) => {
                 event.preventDefault();
-                if(nameInput.val().trim()===""){
+                if (nameInput.val().trim() === "") {
                     window.alert("Rentre ton nom gros débile");
                     return;
                 }
-                
-                if(messageInput.val().trim()===""){
+
+                if (messageInput.val().trim() === "") {
                     window.alert("Tu n'as pas mis de message abruti");
                     return;
                 }
-                
+
                 const auteur = nameInput.val().trim();
                 const contenu = messageInput.val().trim();
 
                 $.ajax({
-                    url:"./enregistrer.php",
+                    url: "./enregistrer.php",
                     method: "GET",
                     data: {
-                        auteur, 
+                        auteur,
                         contenu
                     }
                 })
-                .done((response)=>{
-                    const message = JSON.parse(response);
-                    renderMessage(message);
-                    messageInput.val("");
-                });
+                    .done((response) => {
+                        const message = JSON.parse(response);
+                        renderMessage(message);
+                        messageInput.val("");
+                    });
             })
 
-            setInterval(()=>{
+            setInterval(() => {
                 $.ajax({
-                url:"./recuperer.php",
-                method: "GET"
-            })
-            .done((response)=>{
-                const messages = JSON.parse(response).data;
-                messages.forEach(renderMessage);
-            });
+                    url: "./recuperer.php",
+                    method: "GET"
+                })
+                    .done((response) => {
+                        const messages = JSON.parse(response).data;
+                        messages.forEach(renderMessage);
+                    });
             }, 2000)
         })
     </script>
 </head>
 <body>
-    <input id="name" type="text" placeholder="Entrez votre nom">
-    <ul id="conversation">
-    </ul>
-    <form id="message-form" action="">
-        <input id="message" type="text" placeholder="Entrez un message">
-        <input type="submit" value="ChatPoTer">
-    </form>
+<h1>Bonjour <?= $user ?>, <a href="/">Se deconnecter</a></h1>
+<input id="name" type="text" disabled placeholder="Entrez votre nom" value="<?= $user ?>">
+<ul id="conversation">
+</ul>
+<form id="message-form" action="">
+    <input id="message" type="text" placeholder="Entrez un message">
+    <input type="submit" value="ChatPoTer">
+</form>
 </body>
 </html>
---
+++ www/login.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Se connecter</title>
+</head>
+<body>
+
+<form action="/" method="get">
+    <label>
+        Saisir le nom
+        <input type="text" name="user" placeholder="Entrez un nom">
+    </label>
+
+    <input type="submit" value="Se connecter">
+</form>
+
+</body>
+</html>

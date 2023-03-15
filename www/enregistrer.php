1e534af905da624dd68d4b3166af16e049e4fb3b
Matthéo PERELLE <mattheo.perelle@gmail.com>
2023-03-15 21:19:05 +0100

    check if author is the same as the connected user

--- www/enregistrer.php
+++ www/enregistrer.php
@@ -1,6 +1,17 @@
 <?php
 require_once '../src/Database.php';
+session_start();
 
+// On vérifie que l'utilisateur est bien connecté
+if ($_SESSION['user'] === null) {
+    http_response_code(401);
+    echo(json_encode([
+        'error' => 'Unauthorized'
+    ]));
+    die();
+}
+
+// On vérifie que l'utilisateur est bien l'auteur du message
 if (!(isset($_GET['auteur']))) {
     http_response_code(400);
     echo(json_encode([
@@ -8,6 +19,17 @@ if (!(isset($_GET['auteur']))) {
     ]));
     die();
 }
+
+// On vérifie que l'utilisateur est bien l'auteur du message
+if ($_SESSION['user']['username'] !== $_GET['auteur']) {
+    http_response_code(401);
+    echo(json_encode([
+        'error' => 'Unauthorized'
+    ]));
+    die();
+}
+
+// On vérifie que le contenu du message n'est pas vide
 if (!(isset($_GET['contenu']))) {
     http_response_code(400);
     echo(json_encode([
@@ -16,6 +38,7 @@ if (!(isset($_GET['contenu']))) {
     die();
 }
 
+// On vérifie que le contenu du message n'est pas vide
 if (!(isset($_GET['room_id']))) {
     http_response_code(400);
     echo(json_encode([
@@ -30,6 +53,7 @@ $auteur = htmlspecialchars($_GET['auteur']);
 $contenu = htmlspecialchars($_GET['contenu']);
 $roomID = (int)$_GET['room_id'];
 
+// On enregistre le message dans la base de données
 $req = $pdo->prepare("INSERT INTO chat(date_envoi, auteur, contenu, room_id) VALUES(:date_envoi, :auteur, :contenu, :room_id)");
 $req->execute([
     'date_envoi' => $date,
@@ -38,6 +62,7 @@ $req->execute([
     'room_id' => $roomID
 ]);
 
+// On renvoie le message en JSON
 echo(json_encode([
     'id' => (int)$pdo->lastInsertId(),
     'date_envoi' => $date,
